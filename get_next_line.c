6e30253d00ee98964c14b44c2af530d76cee91a8
Cristina Manzanares Urrutia <crmanzan@car14s4.42barcelona.com>
2023-10-29 11:57:13 +0100

    sin norminette

--- get_next_line.c
+++ get_next_line.c
@@ -6,76 +6,43 @@
 /*   By: crmanzan <crmanzan@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 16:09:10 by crmanzan          #+#    #+#             */
-/*   Updated: 2023/10/27 13:49:11 by crmanzan         ###   ########.fr       */
+/*   Updated: 2023/10/29 11:39:33 by crmanzan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *free_all(char **storage)
+char	*free_all(char **storage)
 {
-    free(*storage);
-    *storage = NULL;
-    return (NULL);
+	free(*storage);
+	*storage = NULL;
+	return (NULL);
 }
 
-static char    *update_storage(char *storage)
+static char	*update_storage(char *storage)
 {
-    char    *new;
-    int     i;
+	char	*new;
+	int		i;
 
-    i = 0;
-    if (!storage)
-        return (NULL);
-    while (storage[i] != '\n' && storage[i] != '\0')
-        i++;
-    i++;
-    new = ft_substr(storage, i, ft_strlen(storage));
-//    printf("storage is %s\n", storage);
-    free(storage);
- //   printf("new is %s\n", new);
-    return (new);
-}
-
-static char    *get_storage(int fd, char *storage)
-{
-    char    *buffer;
-    int     num_bytes;
-    
-    buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    if (!buffer)
-        return (free_all(&storage));
-    buffer[0] = '\0';
-    num_bytes = 1;
-    while (num_bytes > 0 && !ft_strchr(buffer, '\n'))
-    {
-        num_bytes = read(fd, buffer, BUFFER_SIZE);
-        if (num_bytes == -1)
-        {
-            free(buffer);
-            return(free_all(&storage));
-        }
-        if (num_bytes > 0)
-        {
-            //printf("fahdsjkhh");
-            buffer[num_bytes] = '\0';
-            storage = ft_strjoin(storage, buffer);
-        }
-    }
-    free(buffer);
-    //printf("%s\n", storage);
-    return (storage);
-
-}
-/*
-char	*get_storage(int fd, char *storage)
-{
-	int		num_bytes;
-	char	*buffer;
-
-	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if (!buffer)
+	i = 0;
+	if (!storage)
 		return (NULL);
+	while (storage[i] != '\n' && storage[i] != '\0')
+		i++;
+	i++;
+	new = ft_substr(storage, i, ft_strlen(storage));
+	free(storage);
+	return (new);
+}
+
+static char	*get_storage(int fd, char *storage)
+{
+	char	*buffer;
+	int		num_bytes;
+
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (free_all(&storage));
 	buffer[0] = '\0';
 	num_bytes = 1;
 	while (num_bytes > 0 && !ft_strchr(buffer, '\n'))
@@ -84,7 +51,7 @@ char	*get_storage(int fd, char *storage)
 		if (num_bytes == -1)
 		{
 			free(buffer);
-			return (NULL);
+			return (free_all(&storage));
 		}
 		if (num_bytes > 0)
 		{
@@ -94,43 +61,40 @@ char	*get_storage(int fd, char *storage)
 	}
 	free(buffer);
 	return (storage);
-}*/
-
-static char    *the_line(char *storage)
-{
-    char    *line;
-    int     i;
-
-    i = 0;
-    if (!storage)
-        return (NULL);
-    while (storage[i] != '\n' && storage[i] != '\0')
-        i++;
-    i++;
-    line = ft_substr(storage, 0, i);
-    return (line);
-    
 }
 
-char    *get_next_line(int fd)
+static char	*the_line(char *storage)
 {
-    static char *storage = NULL;
-    char        *line;
-    
-    if(fd < 0 || BUFFER_SIZE <= 0)
-        return (NULL);
-    storage = get_storage(fd, storage);
-    if (!storage)
-        return (NULL);
-    line = the_line(storage);
-    if (!line)
-    {
-        return (free_all(&storage));
-    }
- //   printf("old storage is : %s\n", storage);
-    storage = update_storage(storage);
-//    printf("new storage is : %s\n", storage);
-    return (line);
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (!storage)
+		return (NULL);
+	while (storage[i] != '\n' && storage[i] != '\0')
+		i++;
+	i++;
+	line = ft_substr(storage, 0, i);
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*storage = NULL;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	storage = get_storage(fd, storage);
+	if (!storage)
+		return (NULL);
+	line = the_line(storage);
+	if (!line)
+	{
+		return (free_all(&storage));
+	}
+	storage = update_storage(storage);
+	return (line);
 }
 
 /*int main()
